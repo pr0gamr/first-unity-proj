0c5abc70de98b5ec6be29867e89a4a2953563751
Matthew Ertman <ertmm411@hsd.k12.or.us>
2023-09-12 14:28:18 -0700

    added looking around and movement

---
+++ Assets/jump.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+ public Rigidbody rb;
+
+    private float time = 0.0f;
+    private bool isMoving = false;
+    private bool isJumpPressed = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isJumpPressed = Input.GetButtonDown("Jump");
+    }
+
+    void FixedUpdate()
+    {
+        if (isJumpPressed)
+        {
+            // the cube is going to move upwards in 10 units per second
+            rb.velocity = new Vector3(0, 10, 0);
+            isMoving = true;
+            Debug.Log("jump");
+        }
+
+        if (isMoving)
+        {
+            // when the cube has moved for 10 seconds, report its position
+            time = time + Time.fixedDeltaTime;
+            if (time > 1.0f)
+            {
+                Debug.Log(gameObject.transform.position.y + " : " + time);
+                time = 0.0f;
+            }
+        }
+    }
+}
