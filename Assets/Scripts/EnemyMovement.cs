fe349105024b4ba12d7a7558fbd40d8023fdedce
Matthew Ertman <ertmm411@hsd.k12.or.us>
2024-01-12 12:03:03 -0800

    added beginning of enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public NavMeshAgent Enemy;
+    public Transform Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy.SetDestination(Player.position);
+        Debug.Log(Player.position);
+    }
+}
